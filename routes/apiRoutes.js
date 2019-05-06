26866dd45d698903e9a0ccacd06d8caaf3eca95d
gottagoglo <Gloriann@Glorias-iMac.local>
2019-05-06 07:10:51 -0400

    update files prepare for deployment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,9 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
 var friendData = require("../data/friends.js");
-
 
 // ===============================================================================
 // ROUTING
@@ -21,19 +19,23 @@ module.exports = function (app) {
     // ---- POST result for each friend from survey ----
     app.post("/api/friends", function (req, res) {
       console.log("survey has been submitted")
+
+
       var surveyInput = req.body;
       res.json(true);
       console.log("\nName: " + surveyInput.name + "\nPhoto: " +
         surveyInput.photo + "\nScores: " + surveyInput.answers);
 
+  // The split() method is used to split a string into an array of substrings, and returns the new array.
       surveyInput.answers = surveyInput.answers.split(",");
 
       friendData.push(surveyInput);
+
+      // define function to change format of survey answers from strings
       convertAnswers(surveyInput);
       // console.log(friendInput.answers);
 
       compareFriends(friendData, surveyInput);
-
 
     });
   }
@@ -52,7 +54,8 @@ module.exports = function (app) {
       var current = currentFriend;
       // console.log(current);
 
-      var curAnswers = current.answers; // holds answers
+      // variable to hold current answers
+      var curAnswers = current.answers;
 
       // converts answers from strings to numbers
       for (i = 0; i < curAnswers.length; i++) {
@@ -64,17 +67,19 @@ module.exports = function (app) {
     // Comparison function to compare the current friend's answers to those
     // of other friends in the list
     function compareFriends(allFriends, currentFriend) {
+
+      // variables to hold different scores as match is found
       var curFriend = currentFriend.answers;
       var matchFriend;
       var matchScores = [];
       var matchScore = 0;
       var closestMatch;
 
-      // for each friend (excluding last added)...
+      // for loop for each friend (excluding last added)...
       for (i = 0; i < allFriends.length - 1; i++) {
         // store all the scores in an array...
         matchFriend = allFriends[i].answers;
-        // console.log(matchFriend);
+        console.log(matchFriend);
 
         // for each answer in an array...
         for (j = 0; j < matchFriend.length; j++) {
@@ -84,26 +89,27 @@ module.exports = function (app) {
           // the total match score is equal to the sum of all qScores
           matchScore += qScore;
         } // End of scoring for loop
-        // console.log(matchScore)
+        console.log(matchScore)
 
         // push this friends matchScore into an array
         matchScores.push(matchScore);
         // reset the matchScore to zero before moving to the next friend
         matchScore = 0;
+        // curFriend = "";
       } // END of main for loop
-      // console.log(matchScores)
+      console.log(matchScores)
 
       // Find lowest score in matchScores array
       var lowestScore = Math.min(...matchScores);
-      // console.log(lowestScore);
+      console.log(lowestScore);
 
       // find the index of the lowest score
       var matchIndex = matchScores.indexOf(lowestScore);
-      // console.log(matchIndex);
+      console.log(matchIndex);
       // find the friend at this index in the allFriends array
       var bestMatch = allFriends[matchIndex];
 
       // add a new property to the current friend's object that holds the best match
       currentFriend.bestie = bestMatch;
-      // console.log(currentFriend);
+      console.log(currentFriend);
     }
--- server.js
+++ server.js
@@ -4,12 +4,8 @@
 // ==============================================================================
 
 var express = require("express");
-// var exphbs = require("express-handlebars");
-// var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var path = require("path");
-
-
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -22,47 +18,19 @@ var app = express();
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-
-//   // Your port; if not 3306
-//   port: 3306,
-
-//   // Your actionname
-//   user: "root",
-
-//   // User will need to provide their own MySQL password
-//   password: "Guldschool1",
-//   database: "friends_db"
-// });
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Expose the public directory to access CSS files
-app.use(express.static(path.join(__dirname, './app/public')));
+// Expose the public directory to access image files
+app.use(express.static(path.join(__dirname, '/public')));
 
 // ---- Sets up the Express app to handle data parsing ----
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
 
 
 // ================================================================================
